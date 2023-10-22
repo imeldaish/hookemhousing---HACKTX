d20c85fca90895282fdee21e5d3b76fe3aad133b
Kevin Evans <kevinevans@Kevins-MacBook-Air.local>
2023-10-22 01:15:57 -0500

    adding image

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { getQuery } from './api/firebase';
 import { getQuery2 } from './api/firebase';
 import { getQuery3 } from './api/firebase';
 import { getQuery4 } from './api/firebase';
+import myImage from './group1.png';
+
 
 function App() {
     const [apartmentData, setApartmentData] = useState(null)
@@ -56,6 +58,11 @@ function App() {
                         <path d="M23.5 23.5L16 16M1 9.75C1 10.8991 1.22633 12.0369 1.66605 13.0985C2.10578 14.1601 2.7503 15.1247 3.56282 15.9372C4.37533 16.7497 5.33992 17.3942 6.40152 17.8339C7.46312 18.2737 8.60093 18.5 9.75 18.5C10.8991 18.5 12.0369 18.2737 13.0985 17.8339C14.1601 17.3942 15.1247 16.7497 15.9372 15.9372C16.7497 15.1247 17.3942 14.1601 17.8339 13.0985C18.2737 12.0369 18.5 10.8991 18.5 9.75C18.5 8.60093 18.2737 7.46312 17.8339 6.40152C17.3942 5.33992 16.7497 4.37533 15.9372 3.56282C15.1247 2.7503 14.1601 2.10578 13.0985 1.66605C12.0369 1.22633 10.8991 1 9.75 1C8.60093 1 7.46312 1.22633 6.40152 1.66605C5.33992 2.10578 4.37533 2.7503 3.56282 3.56282C2.7503 4.37533 2.10578 5.33992 1.66605 6.40152C1.22633 7.46312 1 8.60093 1 9.75Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                     </svg>
                 </div>
+                <a href="">
+                <div >
+                    <img id="profile-img" src={myImage} alt="pfp" />
+                </div>
+                </a>
             </div>
             <h1 id="title">Explore</h1>
 
